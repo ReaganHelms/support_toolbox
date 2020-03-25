33bfe413e677c97f205e8844687f3e8d25e34ff2
dougbevis <douglas.e.bevis@gmail.com>
2020-03-25 00:25:09 -0400

    Added tab styling and second tab to display the current mobile app versions available in iOS store.

--- warning_v2/assets/main.js
+++ warning_v2/assets/main.js
@@ -1,6 +1,6 @@
 $(function() {
   var client = ZAFClient.init();
-  client.invoke('resize', { width: '100%', height: '250px' });
+  client.invoke('resize', { width: '100%', height: '275px' });
   
   client.get('ticket.requester.id').then(
     function(data) {
